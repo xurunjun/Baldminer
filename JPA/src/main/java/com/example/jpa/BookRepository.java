e90b1eae25776d08b875627670aa9840c4cc9c22
MFk <1814569697@qq.com>
2021-11-28 16:58:31 +0000

    删除文件 JPA

--- JPA/src/main/java/com/example/jpa/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-
-public interface BookRepository extends JpaRepository<Book,String>{
-//    public String<Book> findByLibrary_name(String libraryname);
-}
--- JPA/src/main/java/com/example/jpa/LibraryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface LibraryRepository extends JpaRepository<Library,String> {
-    public List<Library> findByBook_name(String bookName);
-}
