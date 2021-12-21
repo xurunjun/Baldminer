34d1e9863fafc35e3dea7f3097f4f700e363be2f
Pushfct <2066547417@qq.com>
2021-12-21 17:20:46 +0800

    增加帧率限制

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,6 +38,7 @@ public class GameManager : Singleton<GameManager>
     {
         Random.InitState(seed);
         scoreEvent = new List<ScoreEvent>();
+        Applicantion.targetFrameRate = 30;
         scoreEvent.Add(new ScoreEvent());
         scoreEvent.Add(new ScoreEvent());
         scoreEvent.Add(new ScoreEvent());
