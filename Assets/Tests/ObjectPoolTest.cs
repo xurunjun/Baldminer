ac23c3747dadefdde929e92d97f37c46c2d65300
MFk <1814569697@qq.com>
2021-12-22 15:26:56 +0800

    update ObjectPoolTest

--- Assets/Tests/ObjectPoolTest.cs
+++ Assets/Tests/ObjectPoolTest.cs
@@ -7,33 +7,56 @@ using UnityEngine.TestTools;
 
 public class ObjectPoolTest
 {
+    List<GameObject> gameObjects;
+
+    [OneTimeSetUp]
+    public void steup()
+    {
+        gameObjects = new List<GameObject>();
+
+        gameObjects.Add(Resources.Load<GameObject>("Prefabs/Numbers/Number"));
+        gameObjects.Add(Resources.Load<GameObject>("Prefabs/Bullets/addBullet"));
+        gameObjects.Add(Resources.Load<GameObject>("Prefabs/Effect/Explose"));
+    }
 
     [Test]
-    public void ObjectPoolDirTest()
+    public void ObjectPoolDirTest([NUnit.Framework.Range(1,3,1)] int size)
     {
-        ObjectPool objectPool = ObjectPool.Instance;
+        ObjectPool objectPool = new ObjectPool();
 
+        for (int i = 0; i < size; i++)
+        {
+            objectPool.PushObject(gameObjects[i]);
+        }
 
-        GameObject gameObject = Resources.Load<GameObject>("Prefabs/Numbers/Number");
-
-        objectPool.PushObject(gameObject);
-
-        Assert.AreEqual(1,objectPool.getPoolSize());
+        int count = objectPool.getPoolSize();
+        Assert.AreEqual(count,size);
+        Debug.Log(count);
     }
     [Test]
-    public void ObjectPoolQueueTest()
+    [Sequential]
+    public void ObjectPoolQueueTest([Random(0,2,10)] int index,[Random(0,100,10)] int size)
     {
-        ObjectPool objectPool = ObjectPool.Instance;
+        ObjectPool objectPool = new ObjectPool();
+
+        for(int i=0;i<size;i++)
+        {
+            objectPool.PushObject(gameObjects[index]);
+        }
+
+        int count = objectPool.getPoolQueueSize(gameObjects[index]);
+        Assert.AreEqual(count,size);
+        Debug.Log(count);
 
 
-        GameObject gameObject = Resources.Load<GameObject>("Prefabs/Numbers/Number");
+        for(int i=0;i<size;i++)
+        {
+            objectPool.GetObject(gameObjects[index]);
+        }
 
-        objectPool.PushObject(gameObject);
+        count = objectPool.getPoolQueueSize(gameObjects[index]);
+        Assert.AreEqual(0,count);
+        Debug.Log(count);
 
-        Assert.AreEqual(1,objectPool.getPoolQueueSize(gameObject));
-
-        GameObject pullObject = objectPool.GetObject(gameObject);
-
-        Assert.AreEqual(0,objectPool.getPoolQueueSize(gameObject));
     }
 }
